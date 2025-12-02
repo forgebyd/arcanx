3f6c9611a2cf5296ee84a6fe7de5b29f238d0ebd
Dwi Payana <ini.dwiii@gmail.com>
2025-12-03 00:39:16 +0800

    feat(package/utilities): introduce log helper :sparkles:
    Signed-off-by: Dwi Payana <ini.dwiii@gmail.com>

--- packages/utilities/source/index.ts
+++ packages/utilities/source/index.ts
@@ -2,5 +2,6 @@ export * from './consts/error.js';
 
 export * from './helpers/error.js';
 export * from './helpers/filesystem.js';
+export * from './helpers/log.js';
 export * from './helpers/path.js';
 export * from './helpers/result.js';
