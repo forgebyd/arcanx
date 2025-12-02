df174b0e81c189368b348347cf6a296b810073ad
Dwi Payana <ini.dwiii@gmail.com>
2025-12-03 00:26:08 +0800

    feat(package/utilities): introduce path helper :sparkles:
    Signed-off-by: Dwi Payana <ini.dwiii@gmail.com>

--- packages/utilities/source/index.ts
+++ packages/utilities/source/index.ts
@@ -2,4 +2,5 @@ export * from './consts/error.js';
 
 export * from './helpers/error.js';
 export * from './helpers/filesystem.js';
+export * from './helpers/path.js';
 export * from './helpers/result.js';
